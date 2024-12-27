f28bd8fe170ac37a4bbf4ad44f5732fa4879c875
ktzboy <pongpol.sritalanut@gmail.com>
2024-12-27 15:14:36 +0700

    complete

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -19,3 +19,38 @@ int main(){
     cout << "Min = " << B[5];
     return 0;
 }
+
+void stat(const double A[], int N, double B[]) {
+    double sum = 0;
+    for(int i=0 ; i<N ; i++) {
+        sum += A[i];
+    }
+    B[0] = sum / N;
+    
+    sum = 0;
+    for(int i=0 ; i<N ; i++) {
+        sum += pow(A[i]-B[0], 2);
+    }
+    B[1] = sqrt(sum / N);
+    
+    sum = 1;
+    for(int i=0 ; i<N ; i++) {
+        sum *= A[i];
+    }
+    B[2] = pow(sum, 1.0/N);
+    
+    sum = 0;
+    for(int i=0 ; i<N ; i++) {
+        sum += 1/A[i];
+    }
+    B[3] = N / sum;
+    
+    double max = A[0], min = A[0];
+    sum = 1;
+    for(int i=0 ; i<N ; i++) {
+        if(A[i] > max) max = A[i];
+        if(A[i] < min) min = A[i];
+    }
+    B[4] = max;
+    B[5] = min;
+}
